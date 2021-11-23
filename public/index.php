50fceac0116e5a9f2129bfe64065a67b9d632dba
Anni Hopeela <anni.hopeela@edu.sasky.fi>
2021-11-23 14:35:56 +0200

    tapahtumat haetaan tietokannasta sivupohjaan

--- public/index.php
+++ public/index.php
@@ -5,17 +5,19 @@ require_once '../src/init.php';
   
 $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
 $request = strtok($request, '?');   
+ $templates = new League\Plates\Engine(TEMPLATE_DIR);
 
-$templates = new League\Plates\Engine(TEMPLATE_DIR);
+ if ($request === '/' || $request === '/tapahtumat') {
+  require_once MODEL_DIR . 'tapahtuma.php';
+  $tapahtumat = haeTapahtumat();
+  echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
+} 
+    else if ($request === '/tapahtuma') {
+    echo $templates->render('tapahtuma');
+  } else {
+    echo $templates->render('notfound');
+  }
 
-
-    if ($request === '/' || $request === '/tapahtumat') {
-      echo $templates->render('tapahtumat');
-    } else if ($request === '/tapahtuma') {
-      echo $templates->render('tapahtuma');
-    } else {
-      echo $templates->render('notfound');
-    }
   
 
 ?> 
---
+++ src/model/tapahtuma.php
@@ -0,0 +1,10 @@
+<?php
+
+  require_once HELPERS_DIR . 'DB.php';
+
+  function haeTapahtumat() {
+    return DB::run('SELECT * FROM bp_tapahtuma ORDER BY tap_alkaa;')->fetchAll();
+  }
+
+?>
+ 
