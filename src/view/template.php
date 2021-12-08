a845807689d96a4ffc2ac36e318bcb719085a91e
Anni Hopeela <anni.hopeela@edu.sasky.fi>
2021-12-08 10:12:56 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -7,7 +7,20 @@
   </head>
   <body>
   <header>
+  <header>
       <h1><a href="<?=BASEURL?>">Parkki Business Park</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
+    </header>
+
     </header>
 
     <section>
